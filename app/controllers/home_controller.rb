9a8919964c6a0ade9d5aa6676179bd6cb9d5d4b6
Mathieu Robardey <mathieu@altagem.com>
2015-05-19 15:26:35 +0200

    cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!, except: :login
-
-  def index
-    @organizations = current_user.organizations.all
-  end
 
   def login
     if user_signed_in?
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,22 @@
 class OrganizationsController < ApplicationController
+  def index
+    @organizations = current_user.organizations.all
+    respond_to do |format|
+      format.html
+      format.json {
+        render @organizations
+      }
+    end
+  end
+
   def show
-    @organization = Organization.find_by login: params[ :org_login ]
+    @organization = Organization.find_by login: params[ :id ]
+
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: @organization
+      }
+    end
   end
 end
